a729a59d7aa682f50d5aea3ff54be1d7d08c315a
Pear <flippyloveskelly2@gmail.com>
2024-10-07 13:56:51 -0600

    Fixes

--- tools/run_cmi.bat
+++ tools/run_cmi.bat
@@ -1,2 +1,3 @@
-@echo off 
-start /B powershell -WindowStyle Hidden -Command "Add-Type -Path '../../ConvergenceER/Convergence/CMI.dll'; [CMI.CMI]::Main()" 
+@echo off
+taskkill /im powershell.exe /f >nul
+start /B powershell -WindowStyle Hidden -Command "Add-Type -Path '../../ConvergenceER/Convergence/CMI.dll'; [CMI.CMI]::Main()"
