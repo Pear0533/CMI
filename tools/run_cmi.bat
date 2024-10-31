fcc0399c67f5ed42ac0866a3369419681ea7d468
Pear <flippyloveskelly2@gmail.com>
2024-10-30 18:53:12 -0600

    Updated post-build event command

--- tools/run_cmi.bat
+++ tools/run_cmi.bat
@@ -1,3 +1,2 @@
-@echo off
-taskkill /im powershell.exe /f >nul
-start /B powershell -WindowStyle Hidden -Command "Add-Type -Path '../../ConvergenceER/Convergence/CMI.dll'; [CMI.CMI]::Main()"
+@echo off 
+start /B powershell -WindowStyle Hidden -Command "Add-Type -Path '../../ConvergenceER/Convergence/CMI.dll'; [CMI.CMI]::Main()" 
